2652b8fb2f30ec78ba10db764043bb73bc5e3117
martin laville <spi2124@ic2s114-13.univ-lemans.fr>
2015-10-16 10:06:00 +0200

    Fonction de génération de la grille

---
+++ lib_custom.h
@@ -0,0 +1,16 @@
+/* include pour .h*/
+
+#include<stdio.h>
+#include<string.h>
+#define N 50
+
+/* Définition d'un type strct exemple */
+// typedef struct { int jour, mois, an; } t_date;
+
+/* Définition d'un type enum exemple */
+//typedef enum { homme = 1, femme } t_sexe;
+//
+//
+/* Définition de toutes les fonctions dans le bon ordre */
+
+//void ajouter_personne(t_personne * amis, int * nb_amis);
