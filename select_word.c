e3da0348774825a168cfbb6d82de4a9a9c6eb4f6
Drakirus <spi2100@ic2s114-14.univ-lemans.fr>
2015-10-16 10:16:11 +0200

    fonction select

---
+++ select_word.c
@@ -0,0 +1,14 @@
+#define N 4
+#include <stdio.h>
+
+void select_word(){
+	int coord_x,coord_;
+	printf("quelle est la première case : forme (x y)");
+	scanf("%i %i", &coord_x, &coord_y);
+	
+}
+
+int main(){
+	select_word();
+	return 0;
+}
