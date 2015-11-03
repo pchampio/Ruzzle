78ee67c925785dbdf67374973ceadac8a1ad843a
Drakirus <pierrescalade@gmail.com>
2015-11-04 00:06:06 +0100

    PLUS DE GREP ajout d'un arbre prefixer pour la rechreche dans le dico

--- raw.c
+++ raw.c
@@ -1,12 +1,101 @@
-#define N 3
+#define N 4
 #define M (N*N)
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <time.h>
 
 typedef struct{char lettre; int points; char bon[2];}t_case;
+
+
+#define ALPHABET_SIZE 26
+#define  MAX_LENGHT 20
+
+struct t_element {
+    int data;
+    struct t_element* lien_avec[ALPHABET_SIZE];
+};
+
+struct t_element* racine = NULL;
+
+struct t_element*  create_node() {
+    struct t_element *node = (struct t_element*)malloc(sizeof(struct t_element));
+		int x ;
+    for( x = 0;x<ALPHABET_SIZE;x++)
+        node->lien_avec[x] = NULL;
+    node->data = -1;
+    return node;
+}
+
+//                			      racine
+//                            /   \     \
+//    NULL    NULL            m   p      g        --> niveau 0
+//      NULL     NULL         |   |      |
+//     NULL      NULL         a   i      a        --> niveau 1
+//         NULL    NULL       |   |  \   |
+//      NULL      NULL        r   e   x  g        --> niveau 2
+//            NULL           /    |   |
+//                          t    r   e            --> niveau 3
+//         NULL             |    |   |
+//             NULL NULL    i    r   l            --> niveau 4
+//      NULL       NULL     |    |
+//         NULL NULL        n    e                --> niveau 5
+
+// caractères sont implicitement définis par index
+
+
+// c'est comme une file où on ajoute un nœud à la fin de la liste
+// on traverse, (node = node->lien_avec[index] au lieu de node = node->lien_avec) jusnodeu'a  node->lien_avec[index] == NULL (au lieu de node->lien_avec == NULL)
+// nodeuand on atteint NULL, au lieu d'ajoute 1 t_element et faire pointer celuis avent au nouveau, on cree plusieurs t_element (nouvelle lettres de alphaBet)
+// où là valeur est égal au niveau (en bas) donc la longeur d'un mot.
+void insert_node(char character[MAX_LENGHT]) {
+    int length = strlen(character);
+    int index;
+    int level;
+    if(racine == NULL)
+        racine = create_node();
+    struct t_element* node = racine;  // Pour l'insertion de chaque chaîne, nous allons commencer à partir de la racine
+
+    for(level = 0 ; level < length; level++) {
+        // pour chaques level trouver l'index correspondant
+        // characters (a-z = 0-26)
+        int index = character[level] - 'a';
+
+        if(node->lien_avec[index] == NULL) {
+            // mettre la valeur de ce charactersdans node->lien_avec[index]
+            // et on cree un autre t_element qui va pointer vers (node->lien_avec[index] du dessus)
+            node->lien_avec[index] = create_node();  // qui est : struct t_element* suivant = create_node(); node->lien_avec[index] = suivant;
+        }
+
+        node = node->lien_avec[index];
+    }
+    // a la fin de l'arbre node->data est égal à la longeur du mot
+    node->data = level;
+}
+
+int search(char character[MAX_LENGHT]) {
+    struct t_element* node = racine;
+    int length = strlen(character);
+    int level;
+		int index;
+    for(level = 0 ; level < length; level++) {
+        index = character[level] - 'a';
+        if(node->lien_avec[index] != NULL){
+					node = node->lien_avec[index];
+				}else{
+					break;
+				}
+
+    }
+
+		if(character[level] == '\0' && node->data != -1){
+			return node->data;
+		}
+    return -1;
+}
+
+
 
 /* Affiche une matrice */
 void affic_mat(char mat[N][N]){
@@ -75,16 +164,18 @@ void findWordsUtil(char mat[N][N], int visited[N][N], int i, int j, char str[M])
 	// ajoute un char a str
 	visited[i][j] = 1;
 	add_char_to_str(str, mat[i][j]);
-
+	int isfind = search(str);
 	// Si str is present dans le dico, alors il est print
-	if (contains(str, '$', "words.txt"))
-		printf("%s\n", str);
+
+	if (isfind == strlen(str))
+		printf("%s / ", str);
 
 	// Traverse les 8 adjacent cellules de mat[i][j]
 	int dx,dy;
 		for (dx = (i <= 0 ? 0 : -1); dx <= (i >= N-1 ? 0 : 1); dx++) {
 			for (dy = (j <= 0 ? 0 : -1); dy <= (j >= N-1 ? 0 : 1); dy++) {
-				if (!visited[dx+i][dy+j] && contains(str, '.', "words.txt" ) ){ // si la case n'a pas déjà été visiter et qu'il y a un mots avec ce commecement qui existe
+				if (!visited[dx+i][dy+j] ){ // si la case n'a pas déjà été visiter et qu'il y a un mots avec ce commecement qui existe  && contains(str, '.', "words.txt" )
+				// && (isfind == -2 || isfind == strlen(str))
 					findWordsUtil(mat,visited, dx+i, dy+j, str);
 				}
 			}
@@ -110,14 +201,13 @@ void findWords(char mat[N][N]){
 
 }
 
-
 int contains(char grep_obj[M], char EndFlag[1], char File[100]){
 		char command[100];
 		sprintf(command, "grep \"^%s%c\" %s > tmp", grep_obj, EndFlag, File);
 
-		if (EndFlag == '$') {  //debug
-			printf("find --> \"%s\"\n", grep_obj);
-		}
+		// if (EndFlag == '$' ) {  //debug
+		// 	printf("find --> \"%s\"\n", grep_obj);
+		// }
 		system(command);
 
 		char character[20];
@@ -141,6 +231,26 @@ int main(){
 
 	gener_gril(grille);
 	affic_mat(grille);
+
+	char character[MAX_LENGHT];
+	FILE * fic;
+	fic = fopen("./words.txt","r");
+
+	if(fic != NULL){
+
+			fscanf(fic,"%s",character);
+			printf("creation d'un arbre prefixer de : %s ",character);
+			while(!feof(fic)){
+					insert_node(character);
+					// printf("%s\n",character);
+					fscanf(fic,"%s",character);
+
+			}
+			printf("--> %s\n",character);
+	}
+	fclose(fic);
+
+
 	findWords(grille);
 
 	return 0;
